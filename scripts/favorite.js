62773b2ad3b04776f9aa4759ed24ff9bf2a5ac65
Andres Prieto <afprietoa@unal.edu.co>
2023-01-20 19:02:04 -0500

    final project

--- scripts/favorite.js
+++ scripts/favorite.js
@@ -4,8 +4,14 @@ import { printCards } from "../modules/printCards.js";
 const URL_FAVES = 'http://localhost:3000/favorites';
 const container = document.getElementById('favoriteContainer');
 
+
+
 document.addEventListener('DOMContentLoaded', async () => {
     const favorites = await getData(URL_FAVES);
     console.log(favorites);
     printCards(container, favorites)
+    const controllers = document.querySelectorAll('.pos-contrs');
+    const likeIcons = document.querySelectorAll('.card__favorite')
+    controllers.forEach(control => control.style.display = 'none');
+    likeIcons.forEach(icon => icon.style.display = 'none');  
 })
